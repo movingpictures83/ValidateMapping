6cf07738851a9e769a0ca4ff36648468751bfb5e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-07-27 17:55:27 -0400

    Removed python2 paths

--- ValidateMappingPlugin.cpp
+++ ValidateMappingPlugin.cpp
@@ -12,9 +12,10 @@ void ValidateMappingPlugin::run() {
 }
 
 void ValidateMappingPlugin::output(std::string file) {
-   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; validate_mapping_file.py";
+   //std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; validate_mapping_file.py";
+   std::string command = "validate_mapping_file.py";
  command += " -m "+inputfile;
- command += " -o "+file+"; cp "+file+"/*.txt "+file+"/..; export PYTHONPATH=OLDPATH"; 
+ command += " -o "+file+"; cp "+file+"/*.txt "+file+"/.."; 
  std::cout << command << std::endl;
 
  system(command.c_str());
